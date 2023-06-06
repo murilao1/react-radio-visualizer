4c6737cc52874f1b84b8fe01cfb550c245028060
Murilo Nascimento <muriloaugusto580@gmail.com>
2023-06-06 18:51:43 -0300

    build: fix styled-components and add images host

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  compiler: {
+    styledComponents: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "platform-lookaside.fbsbx.com",
+        port: "",
+        pathname: "/platform/profilepic/**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+// https://platform-lookaside.fbsbx.com/platform/profilepic/?asid=832463093574679&height=300&width=300&ext=1688658276&hash=AeQMwHI_Za-SPl0r4oU
