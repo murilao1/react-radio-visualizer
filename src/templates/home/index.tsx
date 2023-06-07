d475bd032cc2765754ad414baabd0aa848108785
Murilo Nascimento <muriloaugusto580@gmail.com>
2023-06-06 22:30:45 -0300

    fix: validate current song playing

--- src/templates/home/index.tsx
+++ src/templates/home/index.tsx
@@ -11,24 +11,26 @@ const HomeTemplate: FC<HomeTemplateProps> = ({ userData, currentSong }) => {
       <Navbar {...userData} />
       <S.Container>
         <S.IsPlaying>{C.isPlaying}</S.IsPlaying>
-        <S.SongContainer>
-          <Link href={currentSong.item.external_urls.spotify} target="_blank">
-            <S.SongImage
-              src={currentSong.item.album.images[0].url}
-              width={424}
-              height={424}
-              alt="Song image"
-              priority
-            />
-            <S.SongName>{currentSong.item.name}</S.SongName>
-          </Link>
-          <Link
-            target="_blank"
-            href={currentSong.item.artists[0].external_urls.spotify}
-          >
-            <S.ArtistName>{currentSong.item.artists[0].name}</S.ArtistName>
-          </Link>
-        </S.SongContainer>
+        {currentSong.is_playing && (
+          <S.SongContainer>
+            <Link href={currentSong.item.external_urls.spotify} target="_blank">
+              <S.SongImage
+                src={currentSong.item.album.images[0].url}
+                width={424}
+                height={424}
+                alt="Song image"
+                priority
+              />
+              <S.SongName>{currentSong.item.name}</S.SongName>
+            </Link>
+            <Link
+              target="_blank"
+              href={currentSong.item.artists[0].external_urls.spotify}
+            >
+              <S.ArtistName>{currentSong.item.artists[0].name}</S.ArtistName>
+            </Link>
+          </S.SongContainer>
+        )}
       </S.Container>
     </>
   );
